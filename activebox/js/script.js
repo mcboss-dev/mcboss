a5b226b6f43dbef28455eee67184119489b981fe
mcboss-dev <62953336+mcboss-dev@users.noreply.github.com>
2021-11-02 22:04:44 +0600

    Add files via upload

---
+++ activebox/js/script.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+
+    /*Nav Toggle */
+    /* ======================================================== */
+    $('.burger').click(function(event)
+    {
+        $('.line-1, .line-2, .line-3, .nav').toggleClass('active');
+    });
+
+
+
+    /* Slider */
+    /* ======================================================== */
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        dots: true
+    });
+
+});
+
+
+window.onload = function( ){
+ let preloader = document.getElementById('preloader');
+ preloader.style.display = 'none';
+};
